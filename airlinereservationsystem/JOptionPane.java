847bc8e16d6d38d30a76553185819540654c3d0f
sharmashikhar9784 <125365779+sharmashikhar9784@users.noreply.github.com>
2023-04-02 19:44:50 +0530

    Project Completed

---
+++ airlinereservationsystem/JOptionPane.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.airlinereservationsystem;
+
+/**
+ *
+ * @author hp
+ */
+class JOptionPane {
+
+    static void showMessageDialog(Object object, String user_Does_Not_exist) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
